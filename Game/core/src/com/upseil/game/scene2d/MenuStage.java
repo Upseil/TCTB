eb6b129198966c3bc20198f8991c670ffdf25849
Lennart Hensler <LennartHensler@gmail.com>
2018-08-27 20:06:56 +0200

    Added the title to the menu

--- Game/core/src/com/upseil/game/scene2d/MenuStage.java
+++ Game/core/src/com/upseil/game/scene2d/MenuStage.java
@@ -4,11 +4,15 @@ import com.artemis.ComponentMapper;
 import com.artemis.World;
 import com.artemis.annotations.Wire;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.upseil.game.Tag;
@@ -38,8 +42,63 @@ public class MenuStage extends Stage {
         
         Table container = new Table(skin);
         container.setFillParent(true);
+        container.top();
+        
+        container.add(new TitleGroup()).expandX().pad(50, 0, 25, 0);
+        container.row();
         container.add(startGameButton);
         addActor(container);
+    }
+    
+    private static class TitleGroup extends WidgetGroup {
+
+        private final int width;
+        private final int height;
+        
+        public TitleGroup() {
+            TextureAtlas titleAtlas = new TextureAtlas("title/title.atlas");
+            
+            AtlasRegion backgroundWhiteRegion = titleAtlas.findRegion("background-white");
+            this.width = backgroundWhiteRegion.originalWidth;
+            this.height = backgroundWhiteRegion.originalHeight;
+            
+            addActor(createAlignedImage(backgroundWhiteRegion));
+            addActor(createAlignedImage(titleAtlas.findRegion("background-black")));
+            for (AtlasRegion region : titleAtlas.getRegions()) {
+                if (region.name.startsWith("filling-")) {
+                    addActor(createAlignedImage(region));
+                }
+            }
+            addActor(createAlignedImage(titleAtlas.findRegion("outlines-stay")));
+            addActor(createAlignedImage(titleAtlas.findRegion("outlines-colorful")));
+        }
+        
+        private Image createAlignedImage(AtlasRegion region) {
+            Image image = new Image(region);
+            image.setPosition(region.offsetX, region.offsetY);
+            return image;
+        }
+
+        @Override
+        public float getPrefWidth() {
+            return width;
+        }
+
+        @Override
+        public float getPrefHeight() {
+            return height;
+        }
+
+        @Override
+        public float getMaxWidth() {
+            return width;
+        }
+
+        @Override
+        public float getMaxHeight() {
+            return height;
+        }
+        
     }
     
 }
